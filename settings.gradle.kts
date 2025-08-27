4f5217967e4c0ec180b4acb6295901cff4d9c871
Sapreet Singh <Sapreet.Singh@autviz.in>
2025-08-27 18:24:00 +0530

    Update project configuration for Gradle, AndroidManifest, and styles; enhance Kotlin plugin versions and adjust permissions

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,18 +1,19 @@
 pluginManagement {
     repositories {
-        google {
-            content {
-                includeGroupByRegex("com\\.android.*")
-                includeGroupByRegex("com\\.google.*")
-                includeGroupByRegex("androidx.*")
-            }
-        }
+        google()
         mavenCentral()
         gradlePluginPortal()
     }
+
+    plugins {
+        id("org.jetbrains.kotlin.android") version "1.9.10"
+        id("com.android.application") version "8.3.0"
+        id("org.jetbrains.kotlin.plugin.parcelize") version "1.9.10"
+    }
 }
+
 dependencyResolutionManagement {
-    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositoriesMode.set(RepositoriesMode.PREFER_SETTINGS)
     repositories {
         google()
         mavenCentral()
@@ -21,4 +22,3 @@ dependencyResolutionManagement {
 
 rootProject.name = "BullsSeeClient"
 include(":app")
- 
